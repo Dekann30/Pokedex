6047ed1453805a991503d0423eda1fd5f7211a0b
Deanna K <keysdeanna@gmail.com>
2022-04-16 19:44:09 -0400

    adds buttons to navigate index and adds button to show page to return to inde.ejs

--- public/app.js
+++ public/app.js
@@ -1,2 +1,10 @@
 console.log('connected static js')
 //only for DOM manipulation
+
+$('#bottom').on('click', ()=>{
+    $('.main')[0].scrollIntoView(false)
+})
+
+$('#top').on('click', ()=>{
+    $('.main')[0].scrollIntoView(true)
+})
